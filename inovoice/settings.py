79705690501fd06608a41766a15103a13f276cda
Yunus Emre Toptancı <y.emretoptanci@gmail.com>
2022-09-09 14:46:05 +0300

    Household Bills

--- inovoice/settings.py
+++ inovoice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-70m1*!y1)+@1g*q_1o(vjcquz6o1z)1ml4j4%1zoc(h-)u3=a*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
